b4aadbe6c08213061ce64ed3d44cfe16d5341371
Julian Schönbächler <info@julian-s.ch>
2019-05-14 09:00:00 +0200

    WIP: Creating 'Create' buttons

--- src/components/editor-panel/Create.vue
+++ src/components/editor-panel/Create.vue
@@ -11,10 +11,26 @@
 
     <div class="property">
       <div>
-        <input type="button" value="Test" />
+        <input class="new-button" type="button" value="Test" />
       </div>
       <div>
-
+        <input class="new-text" type="button" value="Test" />
+      </div>
+    </div>
+    <div class="property">
+      <div>
+        <input class="new-joystick" type="button" value="Test" />
+      </div>
+      <div>
+        <input class="new-dpad" type="button" value="Test" />
+      </div>
+    </div>
+    <div class="property">
+      <div>
+        <input class="new-swipe" type="button" value="Test" />
+      </div>
+      <div>
+        <input class="new-touch" type="button" value="Test" />
       </div>
     </div>
     <div class="property">
@@ -47,3 +63,33 @@ export default {
   name: 'editor-panel-create',
 };
 </script>
+
+<style scoped>
+input[type="button"][class^="new-"] {
+  width: calc(100% - 20px);
+  height: 32px;
+  max-height: 32px;
+  font-size: 16px;
+  background-size: auto 80%;
+  background-position: left 4px center;
+  background-repeat: no-repeat;
+}
+input[type="button"].new-button {
+  background-image: url("../../assets/img/new-button.png");
+}
+input[type="button"].new-dpad {
+  background-image: url("../../assets/img/new-dpad.png");
+}
+input[type="button"].new-joystick {
+  background-image: url("../../assets/img/new-joystick.png");
+}
+input[type="button"].new-swipe {
+  background-image: url("../../assets/img/new-swipe.png");
+}
+input[type="button"].new-text {
+  background-image: url("../../assets/img/new-text.png");
+}
+input[type="button"].new-touch {
+  background-image: url("../../assets/img/new-touch.png");
+}
+</style>
