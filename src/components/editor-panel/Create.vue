3ad1a62ebfe58498e2e46b0de54a2a82bc156cf0
Julian Schönbächler <info@julian-s.ch>
2019-05-16 16:42:51 +0200

    WIP: Create menu Add and removing element data to store

--- src/components/editor-panel/Create.vue
+++ src/components/editor-panel/Create.vue
@@ -1,66 +1,165 @@
 <template>
   <div>
-    <h2>Test</h2>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti ratione est velit?
-      Similique et iste accusantium vitae aspernatur nihil incidunt eveniet ratione quidem.
-      Earum hic provident a, praesentium error facilis!</p>
-    <v-select
-      :value="'Test 1'"
-      :options="[{ value: 'Gaga', options: ['Test 1', 'Test 2', 'huhu'] }]"
-    />
+    <transition name="fade">
+
+      <!-- add view confirmation popup -->
+      <popup v-if="addPopup" title="Add View" @submit="add">
+        <p>
+          Enter a <strong>name identifier</strong> for the new element. The name identifier is the
+          direct connection between Unity and this controller element and will be used to access it
+          from code.
+        </p>
+        <input
+          type="text"
+          v-model="tempElement"
+          @input="sanitize"
+          v-autofocus
+        />
+        <p v-if="addError" class="color-red">
+          {{ addError }}
+        </p>
+        <p>
+          An element name should not contain any special characters with the exception of <b>-</b>
+          (dash) and <b>_</b> (underline).
+        </p>
+        <div class="split right">
+          <input type="button" value="Cancel" @click="addPopup = false" />
+        </div>
+        <div class="split left">
+          <input type="submit" value="OK" />
+        </div>
+      </popup>
+
+    </transition>
+
+    <!-- main panel -->
+    <h2>Create New Element</h2>
 
     <div class="property">
       <div>
-        <input class="new-button" type="button" value="Test" />
-      </div>
-      <div>
-        <input class="new-text" type="button" value="Test" />
-      </div>
-    </div>
-    <div class="property">
-      <div>
-        <input class="new-joystick" type="button" value="Test" />
-      </div>
-      <div>
-        <input class="new-dpad" type="button" value="Test" />
-      </div>
-    </div>
-    <div class="property">
-      <div>
-        <input class="new-swipe" type="button" value="Test" />
-      </div>
-      <div>
-        <input class="new-touch" type="button" value="Test" />
-      </div>
-    </div>
-    <div class="property">
-      <div>
-        <label for="xyz">testlabel</label>
-      </div>
-      <div>
-        <v-select
-          :value="'Test 1'"
-          :options="[{ value: 'Gaga', options: ['Test 1', 'Test 2'] }]"
+        <input
+          class="new-text"
+          type="button"
+          value="Text / Image"
+          @click="addRequest($event, 'text')"
         />
       </div>
+      <div>
+        <input class="new-button" type="button" value="Button" />
+      </div>
     </div>
     <div class="property">
       <div>
-        <label for="xyz">testlabel</label>
+        <input class="new-swipe" type="button" value="Swipe Area" />
       </div>
       <div>
-        <v-select
-          :value="'Test 1'"
-          :options="[{ value: 'Gaga', options: ['Test 1', 'Test 2'] }]"
-        />
+        <input class="new-touch" type="button" value="Touch Area" />
+      </div>
+    </div>
+    <div class="property">
+      <div>
+        <input class="new-dpad" type="button" value="D-Pad" />
+      </div>
+      <div>
+        <input class="new-rdpad" type="button" value="Relative D-Pad" />
+      </div>
+    </div>
+    <div class="property">
+      <div>
+        <input class="new-joystick" type="button" value="Joystick" />
+      </div>
+      <div>
+        <input class="new-rjoystick" type="button" value="Relative Joystick" />
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import sanitizeName from '../../utils/stringSanitizer';
+
 export default {
   name: 'editor-panel-create',
+
+  data() {
+    return {
+      tempElement: null,
+      tempType: null,
+      addPopup: false,
+      addError: null,
+    };
+  },
+
+  computed: {
+    // The controller views
+    views() {
+      return this.$store.getters['controller/views'];
+    },
+
+    // The editors active view name identifier
+    activeView() {
+      return this.$store.getters['editor/activeView'];
+    },
+  },
+
+  methods: {
+
+    // Sanitize 'tempElement' value from special characters
+    sanitize() {
+      const sanitized = sanitizeName(this.tempElement);
+
+      if (sanitized !== false) this.tempElement = sanitized;
+    },
+
+    // Add view button
+    // Event handler
+    addRequest(event = null, type = null) {
+      if (event) event.stopPropagation();
+
+      if (this.addPopup) return;
+
+      this.tempElement = null;
+      this.addError = null;
+      this.tempType = type;
+      this.addPopup = true;
+    },
+
+    // Add view with name
+    add(elements) {
+      if (!this.tempElement || this.tempElement.length < 3) {
+        this.addError = 'The view name identifier must be at least 3 characters long!';
+        elements[0].select();
+        return;
+      }
+
+      const i = this.views.findIndex(v => v.name === this.activeView);
+
+      if (this.views[i].content.some(e => e.name === this.tempElement)) {
+        this.addError = 'An element with this name identifier already exists in the current view!';
+        elements[0].select();
+        return;
+      }
+
+      // Save this action
+      this.$store.dispatch('history/register', {
+        type: 'addElement',
+        activeView: this.activeView,
+      });
+
+      // Adding new element
+      this.$store.commit('controller/addElement', {
+        view: this.activeView,
+        name: this.tempElement,
+        type: this.tempType,
+      });
+      // Set active element
+
+      this.tempElement = null;
+      this.tempType = null;
+      this.addPopup = false;
+    },
+
+  },
 };
 </script>
 
@@ -69,27 +168,32 @@ input[type="button"][class^="new-"] {
   width: calc(100% - 20px);
   height: 32px;
   max-height: 32px;
-  font-size: 16px;
   background-size: auto 80%;
   background-position: left 4px center;
   background-repeat: no-repeat;
 }
 input[type="button"].new-button {
-  background-image: url("../../assets/img/new-button.png");
-}
-input[type="button"].new-dpad {
-  background-image: url("../../assets/img/new-dpad.png");
-}
-input[type="button"].new-joystick {
-  background-image: url("../../assets/img/new-joystick.png");
-}
-input[type="button"].new-swipe {
-  background-image: url("../../assets/img/new-swipe.png");
+  background-image: url("../../assets/img/button-icon.png");
 }
 input[type="button"].new-text {
-  background-image: url("../../assets/img/new-text.png");
+  background-image: url("../../assets/img/text-icon.png");
+}
+input[type="button"].new-dpad {
+  background-image: url("../../assets/img/dpad-icon.png");
+}
+input[type="button"].new-rdpad {
+  background-image: url("../../assets/img/rdpad-icon.png");
+}
+input[type="button"].new-joystick {
+  background-image: url("../../assets/img/joystick-icon.png");
+}
+input[type="button"].new-rjoystick {
+  background-image: url("../../assets/img/rjoystick-icon.png");
+}
+input[type="button"].new-swipe {
+  background-image: url("../../assets/img/swipe-icon.png");
 }
 input[type="button"].new-touch {
-  background-image: url("../../assets/img/new-touch.png");
+  background-image: url("../../assets/img/touch-icon.png");
 }
 </style>
--- src/controllerModule.js
+++ src/controllerModule.js
@@ -103,10 +103,157 @@ export default {
         const i = state.views.findIndex(v => v.name === name);
 
         // If view found, remove it
-        if (i > -1) {
-          state.views.splice(i, 1);
-        }
+        if (i < 0) return;
+
+        state.views.splice(i, 1);
       }
+    },
+
+    // Add a new element to a specified view
+    // Provide a name identifier and a type
+    addElement(state, data = {}) {
+      if (!data.view || !data.name || !data.type) return;
+
+      const i = state.views.findIndex(v => v.name === data.view);
+
+      if (i < 0) return;
+
+      // Create element data
+      let element = {
+        type: data.type,
+        name: data.name,
+        width: 40,
+        height: 40,
+        x: 0,
+        y: 0,
+        hidden: false,
+        layer: 0,
+      };
+
+      const basicElement = {
+        ...element,
+        image: '',
+        imagePosition: 'contain',
+        font: '',
+        fontSize: '',
+        fontStyle: 'regular',
+        fontColor: 'rgb(248, 248, 236)',
+        paddingH: 2,
+        paddingV: 2,
+      };
+
+      // Differentiate between element types
+      switch (data.type) {
+        case 'text':
+          element = {
+            ...basicElement,
+            text: 'Text',
+            textAlignH: 'left',
+            textAlignV: 'top',
+          };
+          break;
+
+        case 'button':
+          element = {
+            ...basicElement,
+            highlightImage: '',
+            text: 'Button',
+            textAlignH: 'center',
+            textAlignV: 'middle',
+          };
+          break;
+
+        case 'swipe':
+          element = {
+            ...basicElement,
+            highlightImage: '',
+            text: 'Swipe Field',
+            textAlignH: 'center',
+            textAlignV: 'middle',
+          };
+          break;
+
+        case 'touch':
+          element = {
+            ...basicElement,
+            highlightImage: '',
+            text: 'Touch Field',
+            textAlignH: 'center',
+            textAlignV: 'middle',
+          };
+          break;
+
+        case 'dpad':
+          element = {
+            ...element,
+            image: '',
+            highlightImageU: '',
+            highlightImageD: '',
+            highlightImageL: '',
+            highlightImageR: '',
+            is8Way: false,
+            imagePosition: 'contain',
+          };
+          break;
+
+        case 'rdpad':
+          element = {
+            ...element,
+            image: '',
+            handlerImage: '',
+            highlightImageU: '',
+            highlightImageD: '',
+            highlightImageL: '',
+            highlightImageR: '',
+            imagePosition: 'contain',
+            is8Way: false,
+            distance: 10,
+          };
+          break;
+
+        case 'joystick':
+          element = {
+            ...element,
+            image: '',
+            stickImage: '',
+            imagePosition: 'contain',
+            distance: 10,
+          };
+          break;
+
+        case 'rjoystick':
+          element = {
+            ...element,
+            image: '',
+            stickImage: '',
+            thumbImage: '',
+            imagePosition: 'contain',
+            stickSize: 0.8,
+            thumbSize: 0.4,
+          };
+          break;
+
+        default:
+          break;
+      }
+
+      state.views[i].content.push(element);
+    },
+
+    // Remove an element from a specified view
+    // Provide a name identifier
+    removeElement(state, view = null, name = null) {
+      if (!view || !name) return;
+
+      const i = state.views.findIndex(v => v.name === view);
+
+      if (i < 0) return;
+
+      const j = state.views[i].content.findIndex(e => e.name === name);
+
+      if (j < 0) return;
+
+      state.views[i].content.splice(j, 1);
     },
   },
 };
